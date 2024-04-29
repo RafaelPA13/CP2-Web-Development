4ca141fc2978e3354aabe75dc64883190bb26997
Matheus Diniz <mtsdnz@hotmail.com>
2024-04-29 17:31:28 -0300

    Corrigido sintaxe e js da pagina maior idade

--- js/maioridade.js
+++ js/maioridade.js
@@ -1,20 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+function sairDoSite() {
+  alert('Desculpe, esse site só é permitido para maiores de 18 anos.');
+  window.location.href = 'https://duckduckgo.com';
+}
 
-    document.getElementById("verificaridade").addEventListener("click", function() {
-        var idade = document.getElementById("idadeInput").value;
-
-        if (idade > 18) {
-            alert("Você é maior de 18 anos. Acesso permitido!");
-        } else {
-            alert("Você é menor de 18 anos. Acesso não permitido.");
-            window.location.href = "https://duckduckgo.com";
-        }
-    });
-
-    // Adicionando a função para redirecionar ao clicar no botão
-    document.getElementById("redirecionar").addEventListener("click", function() {
-        window.location.href = "https://duckduckgo.com";
-    });
-
-});
-
+function irParaLogin() {
+  window.location.href = 'login.html';
+}
