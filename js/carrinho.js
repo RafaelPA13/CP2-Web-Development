d655fb6e96461295c5e2fad787c0b46ade642d0f
Matheus Diniz <mtsdnz@hotmail.com>
2024-04-29 18:03:58 -0300

    Corrige calculo total da pagina de carrinhos

--- js/carrinho.js
+++ js/carrinho.js
@@ -1,31 +1,51 @@
-const precoPorGarrafa = 20
-let qtdTotal = document.querySelector('#qtd-total')
-let qtd = document.querySelector('#qtd')
-let totalVinho = document.querySelector('#total')
-let total = 0
+const precoPorGarrafa = 20;
+const CODIGO_CUPOM_VALIDO = 'FIAP2024';
+let qtdTotal = document.querySelector('#qtd-total');
+let qtd = document.querySelector('#qtd');
+let totalVinho = document.querySelector('#total');
+let total = 0;
+let desconto = 0;
+
+function obterTotal() {
+  return qtd.value * precoPorGarrafa;
+}
+
+function obterTotalComDesconto() {
+  return obterTotal() - desconto;
+}
 
 function recalcular() {
-    let preco = qtd.value * precoPorGarrafa
+  let preco = obterTotal();
+  qtdTotal.innerHTML = preco.toFixed(2);
+  if (desconto > 0) {
+    // Atualiza o desconto sempre que a quantidade de items
+    desconto = preco * 0.1;
+  }
 
-    total += preco
-
-    qtdTotal.innerHTML = preco.toFixed(2)
+  atualizarTotal();
 }
 
 function finalizar() {
-    let cupom = document.querySelector('#cupom').value
+  let cupom = document.querySelector('#cupom').value;
 
-    if (cupom == 'FIAP2024') {
-        total = total - (total * 0.1)
-        alert("Cupom aplicado com sucesso!")
-    } else if (cupom != '') {
-        alert("Digite um cupom válido!")
-        return;
-    } else {
-        alert("Nenhum desconto aplicado! Finalizando Compra!")
-    }
+  if (cupom === CODIGO_CUPOM_VALIDO) {
+    desconto = obterTotal() * 0.1;
+    atualizarTotal();
+    alert('Cupom aplicado com sucesso!');
+  } else if (cupom != '') {
+    alert('Digite um cupom válido!');
+  } else {
+    alert('Nenhum desconto aplicado! Finalizando Compra!');
+  }
+}
 
-    totalVinho.innerHTML = "Total: R$ " + total.toFixed(2)
+function atualizarTotal() {
+  let totalText = 'Total: R$ ' + obterTotalComDesconto().toFixed(2);
+  if (desconto > 0) {
+    totalText += ' (R$ ' + desconto.toFixed(2) + ' de desconto)';
+  }
+
+  totalVinho.innerHTML = totalText;
 }
 
 qtd.addEventListener('input', recalcular);
