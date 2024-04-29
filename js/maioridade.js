f58d844a8c829eb04569703018e1c2f55f129272
Maria Eduarda Cruz <161530778+madu2905@users.noreply.github.com>
2024-04-29 19:41:28 -0300

    melhoria no link, ao ser menor de idade :)

--- js/maioridade.js
+++ js/maioridade.js
@@ -1,6 +1,6 @@
 function sairDoSite() {
   alert('Desculpe, esse site só é permitido para maiores de 18 anos.');
-  window.location.href = 'https://duckduckgo.com';
+  window.location.href = 'https://youtu.be/ykFDE0MHG38?si=RixwA76Ohr2oocho';
 }
 
 function irParaLogin() {
